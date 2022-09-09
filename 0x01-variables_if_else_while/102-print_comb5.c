946ff21855143a7c86ba46e5c2a078b31ec1c5c9
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-09-09 16:47:50 -0700

    done

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations
+ */
+
+int main(void)
+{
+	int z, q;
+
+	for (z = 0; z < 100; z++)
+	{
+		for (q = 0; q < 100; q++)
+		{
+			if (z < q)
+			{
+				putchar((z / 10) + 48);
+				putchar((z % 10) + 48);
+				putchar(' ');
+				putchar((q / 10) + 48);
+				putchar((q % 10) + 48);
+				if (z != 98 || q != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
