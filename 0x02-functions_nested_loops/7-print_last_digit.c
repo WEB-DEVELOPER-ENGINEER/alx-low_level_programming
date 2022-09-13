3604d4cd80b5cf9a9d66137a85e3a60cbbfc9eb7
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-09-13 15:27:24 -0700

    s

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_sign - a function that prints the sign of a number.
+ *
+ * return the last digit
+ *
+ * @j: integer variable
+ */
+
+int print_last_digit(int j)
+{
+	return (j % 10);
+}
