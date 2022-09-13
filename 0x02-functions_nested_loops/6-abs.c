05f575b230494a2653c9dc8efe2f25bb4c268788
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-09-13 15:00:00 -0700

    s

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer.
+ *
+ * Return: 1 for lowercase. 0 for the rest.
+ */
+
+int _abs(int)
+{
+	int n;
+
+	if (n < 0)
+		n = n * -1
+	return (0);
+}
