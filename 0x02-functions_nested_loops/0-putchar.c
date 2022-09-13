a48836b1e9b694dafe0cda248d45b28092ce1257
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-09-13 11:39:09 -0700

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,16 +6,17 @@
  * Return: Always 0 (Success)
  */
 
-int main(void) 
+int main(void)
 {
 	int i = 0;
 	char c[] = "_putchar";
+
 	while (i<8)
-	{	
+	{
 		_putchar(c[i]);
 		i++;
 	}
 	_putchar('\n');
-	
-	return 0;
+
+	return( 0);
 }
