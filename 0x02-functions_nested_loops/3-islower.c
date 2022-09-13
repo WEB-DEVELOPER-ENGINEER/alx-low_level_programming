d7c704ba7715d2ab016b09499da2a13af99d9cba
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-09-13 13:00:26 -0700

    done

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,9 +7,9 @@
 
 int _islower(int c) 
 {
-	if (97 <= c && c >= 122)
+	if (97 <= c && c <= 122)
 		return (1);
-	else if (65 <= c && c >= 90)
+	else if (65 <= c && c <= 90)
 		return (0);
 	else 
 		return (0);
