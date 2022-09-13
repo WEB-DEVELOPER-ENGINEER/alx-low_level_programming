060688db56831790fcc76a72d00f6fdad93f4d9c
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-09-13 11:07:59 -0700

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,14 @@
 
 int main(void) 
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	int i = 0;
+	char c[] = "_putchar";
+	while (i<8)
+	{	
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
 
 	return 0;
 }
