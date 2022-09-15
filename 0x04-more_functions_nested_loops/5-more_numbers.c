0941ee7a621fa28c7dc31bfc4994a288f3c3e9c2
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-09-15 16:35:07 -0700

    s

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,12 +14,8 @@ void more_numbers(void)
 		for (c = 0; c <= 14; c++)
 		{
 			if (c > 9)
-			{
 				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
-			}
-			else
-				_putchar(c);
+			_putchar((c % 10) + '0');
 		}
 	_putchar('\n');
 	}
