9bdc311bd34c499df73357faf342a5dfe5d3c5cf
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-09-15 14:38:40 -0700

    s

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * _isupper - return 1 for
+ * uppercase character. other cases, return 0
+ * @c: The character in ASCII code
+ * Return: 1 for uppercase. 0 for the rest.
+ */
+
+int _isupper(int c)
+{
+	int _islower(int c)
+	{
+		if (65 <= c && 90 >= c)
+			return (1);
+		else
+			return (0);
+	}
+}
