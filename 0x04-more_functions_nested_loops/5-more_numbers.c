3abfb2d3f2726dfa51eaf1b0e36014be6a2ca6f1
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-09-15 16:28:35 -0700

    s

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints from 0 to 14 10 times
+ */
+
+void more_numbers(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c > 9)
+			{	
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+			else
+				_putchar(c);
+		}
+
+	_putchar('\n');
+	}
