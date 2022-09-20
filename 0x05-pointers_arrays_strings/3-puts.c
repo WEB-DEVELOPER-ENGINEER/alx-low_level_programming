30d66b1e24faf7e90af657b9676646519b3a8e79
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-09-20 11:23:04 -0700

    done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - prints string followed by line to stdout
+ * @s: parameter of char type
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i=0; 0 < 1; i++)
+	{
+		if (str[i] != 0)
+		{
+			_putchar(str[i]);
+		}
+		else
+			break;
+	}
+}
