1dffbd98c837714be7e26e9f87f72aa1fac88aa4
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-09-20 15:56:30 -0700

    done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,17 @@
 void print_rev(char *s)
 {
 	int i;
+	int j;
 
 	for (i = 0; 0 < 1; i++)
 	{
-		if (s[i] = 0)
+		if (s[i] == 0)
 			break;
 	}
-	for (i; i >= 0; i--)
-		_putchar(s[i]);
+
+	for (j = i; j >= 0; j--)
+	{	
+		_putchar(s[j]);
+	}
 	_putchar('\n');
 }
