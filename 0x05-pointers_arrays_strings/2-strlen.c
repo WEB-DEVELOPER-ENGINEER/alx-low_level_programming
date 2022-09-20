97845749d792e95c709eb92172d5096674e9111f
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-09-20 10:54:44 -0700

    done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: parameter of char type
+ */
+
+int _strlen(char *s)
+{
+	int counter = 0;
+	int i;
+
+	for (i = 0; 0 < 1; i++)
+	{
+		if (s[i] != 0)
+			counter ++;
+		else
+			break;
+	}
+	return counter;
+}
