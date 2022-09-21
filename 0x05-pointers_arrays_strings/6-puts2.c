8001e767fb499f6584b2e518b1fc4ce87745875c
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-09-20 18:16:13 -0700

    done

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,13 +12,13 @@ void puts2(char *str)
 
 		for (i = 0; 0 < 1; i++)
 		{
-			if (s[i] == 0)
+			if (str[i] == 0)
 			break;
 		}
 		for (j = i; j >= 0; j -= 2)
 		{
-			if (s[j] != 0)
-			_putchar(s[j]);
+			if (str[j] != 0)
+			_putchar(str[j]);
 		}
 		_putchar('\n');
 }
