f52219ab5e120e9ec43e1dd616fc69f03d4550e5
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-09-20 18:15:16 -0700

    done

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character
+ * @str: char parameter
+ */
+
+void puts2(char *str)
+{
+		int i;
+		int j;
+
+		for (i = 0; 0 < 1; i++)
+		{
+			if (s[i] == 0)
+			break;
+		}
+		for (j = i; j >= 0; j -= 2)
+		{
+			if (s[j] != 0)
+			_putchar(s[j]);
+		}
+		_putchar('\n');
+}
