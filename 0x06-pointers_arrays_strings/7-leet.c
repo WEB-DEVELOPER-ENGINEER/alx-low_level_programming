2f535ae68598a566fb32b40c503a80a95c496a3b
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-09-22 11:44:45 -0700

    s

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: char parameter
+ * Return: char
+ */
+
+
+char *leet(char *s)
+{
+	int i;
+	char c[] = "aAeEoOtTlL";
+	int j;
+	char c2[] = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == c[j])
+			{
+				s[i] = c2[j];
+			}
+		}
+	}
+	return (s);
+}
