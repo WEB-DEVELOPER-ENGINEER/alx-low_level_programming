9ff124913966b7726a603919e0319383fca76832
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-09-22 03:52:03 -0700

    done

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings but you
+ * can specify the number of char to be concatenated
+ * from the second string
+ * @dest: char parameter
+ * @src: char parameter
+ * @n: integer parameter
+ * Return: the string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; 0 < 1; i++)
+	{
+		if (dest[i] == 0)
+			break;
+	}
+	for (j = 0; j < n && src[j] != 0; j++)
+		dest[i + j] = src[j];
+	dest[i + j] = '\n';
+	return (dest);
+}
