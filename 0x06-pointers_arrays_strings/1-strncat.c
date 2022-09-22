b2c0647a6a4d1c9cae547317788d9cf5531fc601
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-09-22 04:02:38 -0700

    done

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -22,6 +22,6 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	for (j = 0; j < n && src[j] != 0; j++)
 		dest[i + j] = src[j];
-	dest[i + j] = '\n';
+	dest[i + j + 1] = '\n';
 	return (dest);
 }
