eb394f5c88669cdcb1e5c5dcf582841f309b78c8
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-09-22 08:39:35 -0700

    done

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,12 +13,12 @@ void reverse_array(int *a, int n)
 	int y[20];
 	int x;
 
-	for (i = 0; i <= n / 2; i++)
+	for (i = 0; i <= (n / 2); i++)
 		y[i] = a[i];
 
-	for (j = 0; j < n / 2; j++)
+	for (j = 0; j <= (n / 2); j++)
 		a[j] = a[n - j];
 
-	for (x = 0; x <= n / 2; x++)
+	for (x = 0; x <= (n / 2); x++)
 		a[j + x] = y[x];
 }
