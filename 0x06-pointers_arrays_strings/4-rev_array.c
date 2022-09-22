b92d5a593526d46d2ae771e3cc9b1fdf763a9f3a
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-09-22 08:20:55 -0700

    done

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: integer parameter
+ * @n: integer parameter
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+	int j;
+	int y[8];
+	int x;
+
+	for (i = 0; i <= n / 2; i++)
+		y[i] = a[i];
+
+	for (j = 0; j < n / 2; j++)
+		a[j] = a[n - j];
+
+	for (x = 0; x <= n / 2; x++)
+		a[j + x] = y[x];
+}
