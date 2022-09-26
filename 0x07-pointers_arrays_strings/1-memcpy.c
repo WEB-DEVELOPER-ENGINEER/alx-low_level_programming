cba32d41697468102b60a75f262e810c89d59f0f
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-09-26 06:07:43 -0700

    done

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ *
+ * @dest: char parameter
+ * @src: char parameter
+ * @n: integer parameter
+ * Return: a pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n && *(src + i) != 0; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	*(dest + i) = '\n';
+
+	return (dest);
+}
