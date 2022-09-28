8538143fe018ec3b0fafd1e4dd7ebdcfe78e6212
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-09-28 13:56:42 -0700

    done

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,16 +4,20 @@
  * _sqrt_recursion - returns the natural square root
  * @n: integer parameter
  * Return: natural square root of a number
- * root - computes the root starting from number 1
- * @j: integer parameter
- * @i: integer paramer
- * Return: square root if natural square root, or -1 if not found
  */
 
 int _sqrt_recursion(int n)
 {
 	return (root(n, 1));
 }
+
+/**
+ * root - computes the root starting from number 1
+ * @j: integer parameter
+ * @i: integer paramer
+ * Return: square root if natural square root, or -1 if not found
+ */
+
 int root(int j, int i)
 {
 	int square;
