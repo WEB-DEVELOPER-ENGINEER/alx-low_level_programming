b7b5b1e9ee7c1d843e966f274ebf6d47f4be98c2
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-10-03 12:07:47 -0700

    done

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,7 +28,7 @@ int **alloc_grid(int width, int height)
 				free(arr);
 				for (j = 0; j <= i; j++)
 					free(arr[j]);
-				return(NULL);
+				return (NULL);
 			}
 		}
 		for (i = 0; i < height; i++)
@@ -36,7 +36,7 @@ int **alloc_grid(int width, int height)
 			for (j = 0; j < width; j++)
 				*(*(arr + i) + j) = 0;
 		}
-		return(arr);
+		return (arr);
 	}
 	return (NULL);
 }
