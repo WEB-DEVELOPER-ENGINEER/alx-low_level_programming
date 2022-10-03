f79f0e0568b2f4e935c0d5fa2d897d7f078645ef
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-10-03 07:04:38 -0700

    done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,6 +16,8 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	s = malloc(sizeof(char) * (size));
+	if (size == 0)
+		return (NULL);
 	if (s != NULL)
 	{
 	for (i = 0; i < size; i++)
@@ -25,5 +27,4 @@ char *create_array(unsigned int size, char c)
 	s[i] = '\0';
 	return (s);
 	}
-	return (NULL);
 }
