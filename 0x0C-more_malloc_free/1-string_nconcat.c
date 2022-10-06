3d2109673356badda6442ee1f8e2163b547978a6
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-10-06 16:49:24 -0700

    done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,42 +13,28 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s;
-	unsigned int i, k;
+char *s;
+unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (n <= strlen(s2) && s1 != NULL && s2 != NULL)
-	{
-		s = malloc(strlen(s1) + n + 1);
-		k = strlen(s1) + 1;
-	} else if (n > strlen(s2) && s1 != NULL && s2 != NULL)
-	{
-		s = malloc(strlen(s1) + strlen(s2) + 1);
-		k = strlen(s1) + 1;
-	} else if (s1 != NULL && s2 == NULL)
-	{
-		s = malloc(strlen(s1) + 1);
-		k = strlen(s1) + 1;
-	} else if (s1 == NULL && n <= strlen(s2) && s2 != NULL)
-	{
-		s = malloc(n + 1);
-		k = 0;
-	} else if (s1 == NULL && s2 == NULL)
-	{
-		s = malloc(1);
-		k = 0;
-	}
-	if (s == NULL)
-	{
-		free(s);
-		return (NULL);
-	}
-	for (i = 0; i <= k && s1 != NULL; i++)
-		s[i] = s1[i];
-	for (i = 0; s2[i] != 0 && i <= n; i++)
-	{
-		s[k] = s2[i];
-		k = k + 1;
-	}
-	s[k] = '\0';
-	return (s);
+while (s1 && s1[len1])
+	len1++;
+while (s2 && s2[len2])
+	len2++;
+if (n < len2)
+	s = malloc(sizeof(char) * (len1 + n + 1));
+else
+	s = malloc(sizeof(char) * (len1 + len2 + 1));
+if (!s)
+	return (NULL);
+while (i < len1)
+{
+	s[i] = s1[i];
+	i++;
+}
+while (n < len2 && i < (len1 + n))
+	s[i++] = s2[j++];
+while (n >= len2 && i < (len1 + len2))
+	s[i++] = s2[j++];
+s[i] = '\0';
+return (s);
 }
