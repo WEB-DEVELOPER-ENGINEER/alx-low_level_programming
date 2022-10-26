db4f0cf807f26d0ed625759ba384f8a243ea4511
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-10-25 17:34:12 -0700

    done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	if (head && *head)
 		dum = *head;
-	while (dum && i <= idx)
+	while (dum && i < idx)
 	{
 
 		if (idx == 0)
