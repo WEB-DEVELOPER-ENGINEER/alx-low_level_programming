a2229932e78cf159a9139f8a4ef171f73c489c99
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-11-21 08:52:47 -0800

    done

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to int
+ * @b: char pointer
+ * Return: unsigned int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned res = 0;
+
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		res <<= 1;
+		if (*b & 1)
+			res += 1;
+		b++;
+	}
+	return (res);
+}
