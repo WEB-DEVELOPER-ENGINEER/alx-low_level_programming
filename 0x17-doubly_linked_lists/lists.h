ba86531a62e5edb6687457ab14634d6419af54b6
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-12-08 14:42:53 -0800

    done

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,16 +1,13 @@
-#ifndef list
-#define list
-
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef DDL
+#define DDL
 
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
  * @prev: points to the previous node
  * @next: points to the next node
- *
  * Description: doubly linked list node structure
+ * for Holberton project
  */
 
 typedef struct dlistint_s
@@ -20,7 +17,18 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
-size_t print_dlistint(const dlistint_t *h);
+#include <stdio.h>
+#include <stdlib.h>
 
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+int sum_dlistint(dlistint_t *head);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+int is_palind(void);
+void find_palind(void);
 
 #endif
