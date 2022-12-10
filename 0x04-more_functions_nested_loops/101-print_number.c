6223fb9537684d3409e1bedcd4cb89bbd45edc52
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2022-12-09 17:32:45 -0800

    done

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - prints an integer
+ * @n: an integer
+ */
+
+void print_number(int n)
+{
+	int copy, nth, size = 1, ones = n % 10;
+
+	n /= 10;
+	copy = n;
+	if (ones < 0)
+	{
+		ones *= -1, copy *= -1, n *= -1;
+		_putchar('-');
+	}
+	if (copy > 0)
+	{
+		while (copy / 10 != 0)
+		{
+			copy /= 10, size *= 10;
+		}
+		while (size > 0)
+		{
+			nth = n / size;
+			_putchar('0' + nth);
+			n -= nth * size;
+			size /= 10;
+		}
+	}
+	_putchar('0' + ones);
+}
