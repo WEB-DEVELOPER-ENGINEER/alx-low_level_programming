7a7f55a9c1cf4c1c6faed61ebdae5a94fdcd1dec
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-01-20 08:22:41 -0800

    done

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,10 +9,10 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-        _putchar('-');
-        n = -n;
+		_putchar('-');
+		n = -n;
 	}
-	if(n != 0) 
+	if (n != 0)
 	{
 		print_number(n / 10);
 		_putchar((n % 10) + '0');
