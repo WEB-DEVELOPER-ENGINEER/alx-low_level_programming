81882a8d2c3fa277c1f2a59443e224346b283b5a
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-01-20 08:25:23 -0800

    done

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,11 +12,11 @@ void print_number(int n)
 		_putchar('-');
 		n = -n;
 	}
-	if (n == 0)
-		_putchar('0');
 	if (n != 0)
 	{
 		print_number(n / 10);
 		_putchar((n % 10) + '0');
 	}
+	else
+		_putchar('0');
 }
