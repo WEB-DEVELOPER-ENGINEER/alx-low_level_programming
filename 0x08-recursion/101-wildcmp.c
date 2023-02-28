ddd21a6df0cc5355eaa54b36e4a15d5c401974a3
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-02-28 09:12:09 +0000

    done

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -18,7 +18,7 @@ int wildcmp(char *s1, char *s2)
 		if (*(s2 + 1) == '*')
 			return (wildcmp(s1, s2 + 1));
 		else
-			return (wildcmp(s1 + 1, s2));
+			return (wildcmp(s1, s2));
 	}
 	if (*s1 == '\0')
 	{
