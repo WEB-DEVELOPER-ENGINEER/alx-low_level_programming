65784d4637f00e7fcf11087b7b424d95a0cd98e2
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-04-15 04:13:40 +0000

    done

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: the number
+ * @index: the index, starting from 0 of the bit you want to get
+ * Return: the value of the bit at index index or -1 if an error occured
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	bool res;
+
+	res = n & (1 << index);
+	return (res);
+}
