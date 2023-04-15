f2a6b86885bf55aad61817f89f798c1ba82c9654
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-04-15 14:47:17 +0000

    done

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,32 +10,12 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int i, j = 0, k = 0;
+	unsigned int i = n ^ m, j = 0;
 
-	if (n == m)
-		return (0);
-	i = n & m;
-	while (n)
+	while (i)
 	{
-		j += n & 1;
-		n >>= 1;
+		j += i & 1;
+		i >>= 1;
 	}
-	while (m)
-	{
-		j += m & 1;
-		m >>= 1;
-	}
-	if (i == 0)
-	{
-		return (j);
-	}
-	else
-	{
-		while (i)
-		{
-			k += (i & 1);
-			i >>= 1;
-		}
-	}
-	return (j - k - 1);
+	return (j);
 }
