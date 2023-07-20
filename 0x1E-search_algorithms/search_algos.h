13adbfd747616176a28a43687f98db629d3684c2
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-07-20 10:08:20 -0700

    done

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,5 +10,7 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 void print_array(int *array, size_t l, size_t h);
 int binary_helper(int *array, int value, size_t lo, size_t hi);
+int advanced_binary(int *array, size_t size, int value);
+
 
 #endif
