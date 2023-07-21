e2d5d5d6bd6bceab1912288001cbf33c998077a7
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-07-20 18:02:29 -0700

    done

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,7 +11,6 @@ int exponential_search(int *array, size_t size, int value);
 void print_array(int *array, size_t l, size_t h);
 int binary_helper(int *array, int value, size_t lo, size_t hi);
 int advanced_binary(int *array, size_t size, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value);
 /**
  * struct listint_s - singly linked list
  *
@@ -27,5 +26,5 @@ typedef struct listint_s
 	size_t index;
 	struct listint_s *next;
 } listint_t;
-
+listint_t *jump_list(listint_t *list, size_t size, int value);
 #endif
